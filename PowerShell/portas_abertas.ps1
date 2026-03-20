73169acd95bdeb041d3df237df809e3c83127933
Lucas Lopes <llopes.sec@gmail.com>
2026-03-20 17:06:23 -0300

    Update portas_abertas.ps1

--- PowerShell/portas_abertas.ps1
+++ PowerShell/portas_abertas.ps1
@@ -1,3 +1,8 @@
+# Script: portas_abertas.ps1
+# Descrição: verifica se portas específicas estão abertas em um host
+# Objetivo: identificar serviços expostos na rede
+# Autor: Lucas
+
 $host = "192.168.0.1"
 $ports = 20..25
 
